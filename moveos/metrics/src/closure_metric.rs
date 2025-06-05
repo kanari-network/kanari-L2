ee4984bd8e8fdbeb5e3adc3193b3e338129f57d1
jamesatomc <jiraphonwansai2002@gmail.com>
2025-06-05 07:44:31 +0700

    Update dependencies and versioning across the project
    - Updated Rust version from 1.82.0 to 1.86.0 in Cargo.toml.
    - Changed project version from 0.9.4 to 0.0.1 in Cargo.toml.
    - Updated backtrace dependency from 0.3 to 0.3.75 in Cargo.toml.
    - Upgraded bip32 from 0.4.0 to 0.5.3 in Cargo.toml.
    - Updated clap from 4.5.13 to 4.5.39 in Cargo.toml.
    - Upgraded protobuf from 2.28 to 3.7.2 in Cargo.toml.
    - Updated openrpc.json version from 0.9.4 to 0.9.1.
    - Refactored closure_metric.rs to use `into()` for label_pairs and metric.

--- moveos/metrics/src/closure_metric.rs
+++ moveos/metrics/src/closure_metric.rs
@@ -52,7 +52,7 @@ where
 
     pub fn metric(&self) -> proto::Metric {
         let mut m = proto::Metric::default();
-        m.set_label(protobuf::RepeatedField::from_vec(self.label_pairs.clone()));
+        m.set_label(self.label_pairs.clone().into());
 
         let val = (self.f)().into_f64();
         match self.value_type {
@@ -86,7 +86,7 @@ where
         m.set_name(self.desc.fq_name.clone());
         m.set_help(self.desc.help.clone());
         m.set_field_type(self.value_type.metric_type());
-        m.set_metric(protobuf::RepeatedField::from_vec(vec![self.metric()]));
+        m.set_metric(vec![self.metric()].into());
         vec![m]
     }
 }
